44d8106764f510db3b86a0f7a3a7929ca5e1aa39
Josh <jokajak@gmail.com>
2022-11-03 20:54:36 -0400

    chore: lint fixes

--- lua/keymaster.lua
+++ lua/keymaster.lua
@@ -5,7 +5,7 @@ local M = {}
 local function with_defaults(options)
   return {
     name = options.name or "John Doe",
-    layout = options.layout or "qwerty"
+    layout = options.layout or "qwerty",
   }
 end
 
@@ -30,7 +30,7 @@ local buf, win
 local function open_window(columns, lines)
   buf = api.nvim_create_buf(false, true) -- create new emtpy buffer
 
-  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  api.nvim_buf_set_option(buf, "bufhidden", "wipe")
 
   -- get dimensions
   local width = api.nvim_get_option("columns")
@@ -51,7 +51,7 @@ local function open_window(columns, lines)
     width = win_width,
     height = win_height,
     row = row,
-    col = col
+    col = col,
   }
 
   -- and finally create it with buffer attached
@@ -61,23 +61,23 @@ end
 local function center(str)
   local width = api.nvim_win_get_width(0)
   local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
-  return string.rep(' ', shift) .. str
+  return string.rep(" ", shift) .. str
 end
 
 local function update_view(lines)
   -- Is nice to prevent user from editing interface, so
   -- we should enabled it before updating view and disabled after it.
-  api.nvim_buf_set_option(buf, 'modifiable', true)
+  api.nvim_buf_set_option(buf, "modifiable", true)
 
   -- add a little indentation
-  for i=1, #lines do
-    lines[i] = '  '..lines[i]
+  for i = 1, #lines do
+    lines[i] = "  " .. lines[i]
   end
 
   api.nvim_buf_set_lines(buf, 0, -1, false, {
-      center('Keymaster'),
-      ''
-    })
+    center("Keymaster"),
+    "",
+  })
   api.nvim_buf_set_lines(buf, 2, -1, false, lines)
 end
 
@@ -87,22 +87,21 @@ end
 
 local function move_cursor()
   local new_pos = math.max(3, api.nvim_win_get_cursor(win)[1] - 1)
-  api.nvim_win_set_cursor(win, {new_pos, 0})
+  api.nvim_win_set_cursor(win, { new_pos, 0 })
 end
 -- Display starts on line 3, prevent going above that
 
-function M.display(mode)
+function M.display()
   if not M.is_configured() then
     return
   end
 
-  mode = mode or "normal"
   local keycap_keys = layouts.layouts[M.options.layout]
 
   local layout = layouts.render(keycap_keys)
   open_window(layout.row_length, #layout.layout)
   update_view(layout.layout)
-  api.nvim_win_set_cursor(win, {3, 0})
+  api.nvim_win_set_cursor(win, { 3, 0 })
 end
 
 -- Expose functions
